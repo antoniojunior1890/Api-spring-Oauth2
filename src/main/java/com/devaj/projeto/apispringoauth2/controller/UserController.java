b5d2be85e30df38de178484496b49a7ec704f981
antoniojunior1890 <antonio.junior1890@gmail.com>
2019-09-09 16:04:43 -0300

    crud completo

--- src/main/java/com/devaj/projeto/apispringoauth2/controller/UserController.java
+++ src/main/java/com/devaj/projeto/apispringoauth2/controller/UserController.java
@@ -3,11 +3,14 @@ package com.devaj.projeto.apispringoauth2.controller;
 import com.devaj.projeto.apispringoauth2.entity.User;
 import com.devaj.projeto.apispringoauth2.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -16,8 +19,37 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+
     @GetMapping
-    public List<User> list(){
-        return userRepository.findAll();
+    public Page<User> list(@PageableDefault(page = 0, size = 5)
+                               @SortDefault.SortDefaults({
+                                    @SortDefault(sort = "id", direction = Sort.Direction.ASC)
+    })Pageable pageable) {
+
+        return userRepository.findAll(pageable);
     }
+
+    @PostMapping
+    public User save(@RequestBody User user) {
+
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public User edit(@PathVariable("id") Long id, @RequestBody User user) {
+        User userEdit = user;
+        userEdit.setId(id);
+        return userRepository.save(userEdit);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> detail(@PathVariable("id") Long id) {
+        return userRepository.findById(id);
+    }
+
 }
